5a1e65858a16794987bb4e6cd71962750d092b17
PepperShin <peep1996@naver.com>
2025-03-12 14:54:36 +0900

    commit

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "pybo",  # dev_2 pybo 파일을 미리 메모리에 할당. pybo.apps.PyboConfig와 동일.
 ]
 
 MIDDLEWARE = [
