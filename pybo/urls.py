5a1a2f632e844a263cdf6099c57932be45e6bae9
PepperShin <peep1996@naver.com>
2025-03-17 11:01:52 +0900

    commit

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
-
 from pybo import views
 
+app_name = "pybo"
+
 urlpatterns = [
-    path("", views.index),
-    path("<int:question_id>/", views.detail),  # dev_3
+    path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"),  # dev_3
 ]
