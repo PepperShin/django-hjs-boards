cbeeeb7c5ce808f6efdf99309eb42376e4c4615c
PepperShin <peep1996@naver.com>
2025-03-24 09:18:52 +0900

    commit

--- common/views.py
+++ common/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
+
+from common.forms import UserForm
 
 
 # Create your views here.
 def logout_view(request):
     logout(request)
     return redirect("index")
+
+
+def signup(request):
+    if request.method == "POST":
+        """
+        request.POST.get("username")
+        request.POST.get("password1")
+        request.POST.get("password2")
+        """
+        form = UserForm(request.POST)  # from common.forms import UserForm
+
+        if form.is_valid():  # 유효성 검사
+            form.save()
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)  # 로그인
+
+            return redirect("index")
+
+    else:
+        form = UserForm()
+
+    return render(request, "common/signup.html", {"form": form})
